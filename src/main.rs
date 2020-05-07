6a63aa1f833dd630d56d570d7ac5a371f1c8e424
Travis Matthews <trav.matth@gmail.com>
2020-05-07 03:30:27 -0700

    parse tests

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use std::{env, error::Error, fs, net};
+use std::{
+    env,
+    error::Error,
+    fs,
+    net::{Ipv4Addr, SocketAddrV4},
+};
 
-fn load_opts(opts: Vec<String>) -> Result<(i64, Vec<net::IpAddr>), Box<dyn Error + 'static>> {
+fn load_opts(opts: Vec<String>) -> Result<(SocketAddrV4, Vec<Ipv4Addr>), Box<dyn Error + 'static>> {
     let port = opts[1].parse()?;
-    let data = fs::read_to_string(&opts[2])?;
+    let socket_addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, port);
+    let contents = fs::read_to_string(&opts[2])?;
     let mut servers = Vec::new();
 
-    for server in data.lines().collect::<Vec<_>>() {
-        servers.push(server.parse::<net::IpAddr>()?);
+    for server in contents.lines().collect::<Vec<_>>() {
+        servers.push(server.parse::<Ipv4Addr>()?);
     }
-    Ok((port, servers))
+    Ok((socket_addr, servers))
 }
 
 fn main() {
@@ -17,6 +23,39 @@ fn main() {
         return;
     }
     let args = env::args().collect::<Vec<String>>();
-    let (_, servers) = load_opts(args).unwrap();
-    println!("{:?}", servers);
+    let (socket, servers) = load_opts(args).unwrap();
+    println!("{:?} {:?}", socket, servers);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn to_vec_string(arr: Vec<&str>) -> Vec<String> {
+        arr.into_iter().map(|s| s.to_owned()).collect()
+    }
+
+    #[test]
+    fn load_opts_rejects_invalid_port() {
+        let opts = to_vec_string(vec!["a", "b", "c"]);
+        if let Ok(_) = load_opts(opts) {
+            panic!("load_opts should reject invalid port")
+        }
+    }
+
+    #[test]
+    fn load_opts_rejects_invalid_filename() {
+        let opts = to_vec_string(vec!["lb", "80", "b"]);
+        if let Ok(_) = load_opts(opts) {
+            panic!("load_opts should reject invalid port")
+        }
+    }
+
+    #[test]
+    fn test_load_opts() {
+        let opts = to_vec_string(vec!["lb", "80", "tests/list.txt"]);
+        if let Ok((s, _)) = load_opts(opts) {
+            assert_eq!(s, SocketAddrV4::new(Ipv4Addr::LOCALHOST, 80));
+        }
+    }
 }
